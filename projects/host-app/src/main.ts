a6683f5d850bb995889bb806d94c637c91426620
dbel431 <dbel431@gmail.com>
2026-02-27 11:48:06 +0530

    2

--- projects/host-app/src/main.ts
+++ projects/host-app/src/main.ts
@@ -1,6 +1,8 @@
 import { initFederation } from '@angular-architects/native-federation';
 
-initFederation()
+initFederation({
+  "micro1":"http://localhost:5200/remoteEntry.json"
+})
   .catch(err => console.error(err))
   .then(_ => import('./bootstrap'))
   .catch(err => console.error(err));
